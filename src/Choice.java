9626e43b5d8045722dc14f6c56b5ce0e9e52685a
Adam <adamlukewilkins@gmail.com>
2016-09-02 17:27:25 -0400

    testing collaboration

---
+++ src/Choice.java
@@ -0,0 +1,14 @@
+/**
+ * Created by adam on 9/2/16.
+ */
+
+import java.util.*;
+
+public class Choice {
+    public void input(){
+        Scanner keyboard = new Scanner(System.in);
+        String fateofChoice = keyboard.nextLine();
+        
+
+    }
+}
--- src/Room.java
+++ src/Room.java
@@ -3,9 +3,20 @@
  */
 public class Room implements iRoom {
 
-    public void welcome(){} //description of the room
+    public void welcome(){
+        System.out.println("you walking into a room.");
+        System.out.println("its quiet... to quiet.");
+    } //description of the room
 
-    public void decision(){} //calling the class for userinput and saving
+    public void decision(){
+
+        System.out.println("do you go right, left, or forward?");
+        System.out.print(">");
+
+        Choice choice = new Choice();
+
+
+    } //calling the class for userinput and saving
 
     public void fate(){}  //did you die?
 
